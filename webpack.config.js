fc153280a3f524c80401cac6066bd686538b441e
tmslpm <thomaslepomme@gmail.com>
2025-04-16 02:47:06 +0200

    fix workflow

--- webpack.config.js
+++ webpack.config.js
@@ -8,71 +8,74 @@ const packages = require("./package.json");
 const minimize = false;
 const licenseText = FileSystem.readFileSync(path.resolve(__dirname, "license.txt"), "utf8");
 
-module.exports = {
-  mode: "development",
 
-  entry: (() => {
-    const e = {};
-    glob.sync("./src/**/*.scss").forEach(v => {
-      e[path.relative("./src", v).replace(/\.scss$/, "")] = path.resolve(__dirname, v);
-    });
-    return e;
-  })(),
+module.exports = env => {
+  return {
+    mode: "development",
 
-  output: {
-    path: path.resolve(__dirname, "dist", packages.name),
-    clean: true
-  },
+    entry: (() => {
+      const e = {};
+      glob.sync("./src/**/*.scss").forEach(v => {
+        e[path.relative("./src", v).replace(/\.scss$/, "")] = path.resolve(__dirname, v);
+      });
+      return e;
+    })(),
 
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ],
-      },
+    output: {
+      path: path.resolve(__dirname, env.gh ? "build" : "dist", packages.name),
+      clean: true
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+
+      new webpack.BannerPlugin({
+        banner: licenseText,
+        entryOnly: true,
+        // NOTE : cela ne s’applique qu’aux JS. Les CSS auront besoin d’un autre traitement si tu veux que la licence soit en tête
+      }),
+
+      (function cleanupJsFile() {
+        return {
+          apply: compiler => {
+            compiler.hooks.emit.tapAsync("RemoveJSChunks", (v, cb) => {
+              Object.keys(v.assets)
+                .filter(n => n.endsWith(".js"))
+                .forEach(n => (delete v.assets[n]));
+              cb();
+            });
+          }
+        };
+      })()
     ],
-  },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
+    optimization: {
+      minimize,
+      minimizer: [
+        new CssMinimizerPlugin(),
+      ],
+    },
 
-    new webpack.BannerPlugin({
-      banner: licenseText, 
-      entryOnly: true,
-      // NOTE : cela ne s’applique qu’aux JS. Les CSS auront besoin d’un autre traitement si tu veux que la licence soit en tête
-    }),
+    resolve: {
+      extensions: [
+        ".scss"
+      ]
+    }
 
-    (function cleanupJsFile() {
-      return {
-        apply: compiler => {
-          compiler.hooks.emit.tapAsync("RemoveJSChunks", (v, cb) => {
-            Object.keys(v.assets)
-              .filter(n => n.endsWith(".js"))
-              .forEach(n => (delete v.assets[n]));
-            cb();
-          });
-        }
-      };
-    })()
-  ],
-
-  optimization: {
-    minimize,
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  },
-
-  resolve: {
-    extensions: [
-      ".scss"
-    ]
   }
-
 };
